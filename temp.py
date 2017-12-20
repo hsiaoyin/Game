50da7045ba169e66382438b158fce5b0dd515208
hsiaoyin <ahuaflower@gmail.com>
2017-12-20 16:11:12 +0100

    Practice

---
+++ temp.py
@@ -0,0 +1,16 @@
+#import csv
+import os
+f=open("result.csv","w")
+check=str(list())
+f.write(check)
+f.close
+#with open('result.csv','w',newline="",) as csvfile:
+
+
+#    spamwriter=csv.writer(csvfile,delimiter=",")
+#    spamwriter.writerow(['name','InitialSetup',check])
+#    spamreader=csv.reader(csvfile,delimiter=",")
+#    print(type(spamreader))
+#    print(dir(type(spamreader)))
+#    for row in spamreader:
+#        print(",".join(row))
